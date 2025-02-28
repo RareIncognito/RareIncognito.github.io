f1c57473ba7e6e64477c81db41474a40e7ad8936
Donovan C. <143840393+RareIncognito@users.noreply.github.com>
2025-02-28 18:41:13 +0000

    finish filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -21,8 +21,8 @@ function resetAndRender() {
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
   applyFilter(reddify);
-  applyFilter(decreaseBlue);
-  applyFilter(increaseGreenByBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
+  applyFilterNoBackground(decreaseBlue);
   // do not change the below line of code
   render($("#display"), image);
 }
@@ -32,6 +32,7 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+//Applys a spceific filter to the image
 function applyFilter(filterFunction) {
   for (i = 0; i < image.length; i++) {
     for (j = 0; j < image[i].length; j++) {
@@ -46,27 +47,42 @@ function applyFilter(filterFunction) {
 }
 
 // TODO 7: Create the applyFilterNoBackground function
-
+//Applys the filter to anything that is not the background
+function applyFilterNoBackground() {
+  var backgroundColour = image[0][0]
+  for (i = 0; i < image.length; i++) {
+    for (j = 0; j < image[i].length; j++) {
+      if(image[i][j] ===! backgroundColour){
+        var rgbString = image[i][j]
+      var rgbNumbers = rgbStringToArray(rgbString)
+      filterFunction(rgbNumbers)
+      rgbString = rgbArrayToString(rgbNumbers)
+      image[i][j] = rgbString
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
+//Keeps the filter that is applied in bounds of the image
 function keepInBounds(bound) {
-  let max = (bound < 0) ? 0 : bound;
-  let min = (bound > 255) ? 255 : bound;
+  return (bound < 0) ? 0 : (bound > 255) ? 255 : bound;
 }
 
 // TODO 3: Create reddify function
+//Applys a red filter to image
 function reddify(epic) {
   epic[RED] = 200
 }
 
 // TODO 6: Create more filter functions
 // Reduces the amount BLUE in the RGB array
-function decreaseBlue(blueArray){
+function decreaseBlue(blueArray) {
   blueArray[BLUE] = keepInBounds(blueArray[BLUE] - 50)
 }
 
 //Increase the amount of GREEN in the RGB array in relation to the amount of BLUE in the array.
-function increaseGreenByBlue(greenArray){
+function increaseGreenByBlue(greenArray) {
   greenArray[GREEN] = keepInBounds(greenArray[BLUE] + greenArray[GREEN])
 }
 // CHALLENGE code goes below here
